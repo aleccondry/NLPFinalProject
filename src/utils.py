d7091622fd72def295fc3a67c253679d756546df
Alec Condry <aleccondry@gmail.com>
2023-11-29 17:57:51 -0500

    Started the transformer implementation, updated utils to have a clean_data function that makes a new folder with the data cleaned to only have articles and their headlines

--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,9 @@ def load_article_data(path: str) -> dict[str, pd.DataFrame]:
     """
     article_data = {}
     for filename in os.listdir(path):
-        df = pd.read_csv(os.path.join(path, filename), encoding='utf-8')
-        article_data[filename] = df
+        if "." in filename and filename.split('.')[1] == 'csv':
+            df = pd.read_csv(os.path.join(path, filename), encoding='utf-8')
+            article_data[filename] = df
     return article_data
 
 def clean_data():
@@ -20,4 +21,22 @@ def clean_data():
     To be implemented later
     Will clean data and return dataframes with only valid, important data & consistent column names
     """
+    articles = load_article_data(path='../data/')
+    columns_kept = ["title", "content", "Headline", "Article text", "Article", "Heading"]
+    clean_data_path = '../data/cleaned_data/'
+    if not os.path.exists(clean_data_path):
+        os.makedirs(clean_data_path)
+    for filename, df in zip(articles.keys(), articles.values()):
+        for col in df.columns:
+            if not any(col == ck for ck in columns_kept):
+                df.drop(columns=[col], inplace=True)
+            elif col == "title" or col == "Headline" or col == "Heading":
+                df.rename(columns={col: "headline"}, inplace=True)
+            elif col == "content" or col == "Article text" or col == "Article":
+                df.rename(columns={col: "article"}, inplace=True)
+        if df.columns.tolist()[0] == 'article':
+            df = df[df.columns.tolist()[-1::-1]]
+        print(df.columns)
+        df.to_csv(clean_data_path + "clean_" + filename, index=False)
+        print()
     return
